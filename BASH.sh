b295d64f83d9ed7b48aef1ef091b2fba8c405dc3
NovaOSholder <bennuribaykondu5@gmail.com>
2025-01-12 00:03:03 +0300

    Create BASH.sh

---
+++ BASH.sh
@@ -0,0 +1,4 @@
+sudo apt install openssh-server
+sudo systemctl enable ssh  # SSH servisini başlatmak ve sistem başlangıcında otomatik olarak başlatmak için
+sudo systemctl start ssh   # SSH servisini hemen başlatmak için
+sudo systemctl status ssh  # SSH servisini kontrol etmek için
